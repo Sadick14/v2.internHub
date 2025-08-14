9a37c5c54832537a3a441a8b5d66ba764e9dfa86
Issaka Saddick <issakasaddick14@gmail.com>
2025-08-14 11:51:13 +0000

    okcc ehtv vtkd dwfj internshiphub8@gmail.com
    this is the password and e

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -5,7 +5,7 @@ import 'dotenv/config';
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SERVER_HOST,
   port: Number(process.env.EMAIL_SERVER_PORT),
-  secure: true, 
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_SERVER_USER,
     pass: process.env.EMAIL_SERVER_PASSWORD,
@@ -22,7 +22,7 @@ interface MailOptions {
 }
 
 export async function sendMail({ to, subject, text, html }: MailOptions) {
-  const from = `InternshipTrack <${process.env.EMAIL_SERVER_USER}>`;
+  const from = `InternshipTrack <${process.env.EMAIL_FROM}>`;
 
   await transporter.sendMail({
     from,
