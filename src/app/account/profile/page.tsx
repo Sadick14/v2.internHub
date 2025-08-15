e11ebf5d891eee688e6245c6325f97b7d69add59
Issaka Saddick <issakasaddick14@gmail.com>
2025-08-15 01:27:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/account/profile/page.tsx
+++ src/app/account/profile/page.tsx
@@ -6,7 +6,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { useToast } from '@/hooks/use-toast';
